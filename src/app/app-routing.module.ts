b1f04c24d5f3604a0d5a49da6446c9300f5d0cc9
EeshaArif <eeshaarif@gmail.com>
2021-01-07 01:30:22 +0500

    ref(app): Update routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,22 @@
+import { HomeComponent } from './home/home.component';
+import { MessageComponent } from './message/message.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
   {
-    path:'conversations',
-  
-  }
+    path: '',
+    component: HomeComponent,
+  },
+
+  {
+    path: 'conversations',
+    component: MessageComponent,
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}

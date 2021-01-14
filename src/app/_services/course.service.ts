9135c0d9390ebd3ae8c090fcef781ab94e5cdb3f
EeshaArif <eeshaarif@gmail.com>
2021-01-15 00:02:20 +0500

    perf(service): Make courses dynamic

--- src/app/_services/course.service.ts
+++ src/app/_services/course.service.ts
@@ -1,9 +1,25 @@
+import { Course, CourseResponse } from './../_models/courseModel';
+import { environment } from './../../environments/environment';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Subject, Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CourseService {
+  BASE_URL = `${environment.apiUrl}/courses.php`;
+  courseStore: Course[] = [];
+  courseSubject: Subject<Course[]> = new Subject();
+  courses: Observable<Course[]> = this.courseSubject.asObservable();
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  getAllCourses(): void {
+    this.http
+      .get<CourseResponse>(`${this.BASE_URL}?action=get_courses_list`)
+      .subscribe((courses) => {
+        this.courseStore = courses.courses_list;
+        this.courseSubject.next(this.courseStore);
+      });
+  }
 }
--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -1,15 +1,15 @@
+import { CourseService } from './../_services/course.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
-  styleUrls: ['./courses.component.css']
+  styleUrls: ['./courses.component.css'],
 })
 export class CoursesComponent implements OnInit {
-
-  constructor() { }
+  constructor(public courseService: CourseService) {}
 
   ngOnInit(): void {
+    this.courseService.getAllCourses();
   }
-
 }

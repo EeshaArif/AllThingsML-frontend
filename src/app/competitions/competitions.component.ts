58f6e08c45268eb6bd9634c52c6da5d4c9f07d16
syedbabart <58231237+syedbabart@users.noreply.github.com>
2021-01-26 01:53:10 +0500

    (enhancement): adding addition of competitions facility

--- src/app/competitions/competitions.component.ts
+++ src/app/competitions/competitions.component.ts
@@ -1,5 +1,6 @@
 import { CompetitionService } from './../_services/competition.service';
 import { Component, OnInit } from '@angular/core';
+import { Competition } from '../_models/competitionModel';
 
 @Component({
   selector: 'app-competitions',
@@ -7,9 +8,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./competitions.component.css'],
 })
 export class CompetitionsComponent implements OnInit {
+  competitionForm: Competition = {
+  id: 0,
+  name: '',
+  description: '',
+  start_date: '',
+  end_date: '',
+  image: '',
+  prize: '',
+  host: '',
+  link: '',
+  }
   constructor(public competitionService: CompetitionService) {}
 
   ngOnInit(): void {
     this.competitionService.getAllCompetitions();
   }
+  addCompetition(): void {
+    this.competitionService.postCompetition(this.competitionForm);
+    this.competitionForm.name = '';
+    this.competitionForm.description = '';
+    this.competitionForm.start_date = '';
+    this.competitionForm.end_date = '';
+    this.competitionForm.image = '';
+    this.competitionForm.prize = '';
+    this.competitionForm.host= '';
+    this.competitionForm.link = ''; 
+  }
 }
+
